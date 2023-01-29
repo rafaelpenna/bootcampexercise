8e0317dd294de947976f0e30d9b5c7eac8609cfe
Rafael Penna <ragapen@gmail.com>
2023-01-29 17:15:38 -0300

    initial commit

--- bootcampExercise/ViewController.swift
+++ bootcampExercise/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var adressField: UITextField!
+    @IBOutlet var passwordField: UITextField!
+    @IBOutlet var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        nameField.delegate = self
+        adressField.delegate = self
+        passwordField.delegate = self
+        
+        loginButton.isEnabled = false
     }
 
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        let field = textField
+        field.layer.borderColor = UIColor.blue.cgColor
+        field.layer.borderWidth = 1
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        let field = textField
+        if field.text == "" {
+            field.layer.borderColor = UIColor.red.cgColor
+            field.layer.borderWidth = 2
+        } else {
+            field.layer.borderColor = UIColor.lightGray.cgColor
+            field.layer.borderWidth = 1
+        }
+        
+        validateButton()
+    }
+    
+    
 
+    @IBAction func cadastrateButton(_ sender: Any) {
+        print("Cadastro realizado com sucesso")
+    }
+    
+    private func validateButton() {
+        if nameField.text == "" || adressField.text == "" || passwordField.text == "" {
+            loginButton.isEnabled = false
+        } else {
+            loginButton.isEnabled = true
+        }
+    }
 }
 
